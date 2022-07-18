ea94fbd98a4334c66e3d30cdd047bad577a41c4a
swgarz <swgarz@gmail.com>
2022-07-18 13:39:26 -0500

    description, title & site_name

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Wilson bienes y raíces',
+  title: 'Wilson Bienes y Raíces',
+  description: 'Wilson bienes y raíces, especialistas en compra y venta',
   locale: 'en',
 };
