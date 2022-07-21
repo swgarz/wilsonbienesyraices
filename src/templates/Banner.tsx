d37396db87c6a853441d89edf343212cbdc6b448
swgarz <swgarz@gmail.com>
2022-07-21 00:05:02 -0500

    facebook

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -7,10 +7,10 @@ import { Section } from '../layout/Section';
 const Banner = () => (
   <Section>
     <CTABanner
-      title="Privacidad"
-      subtitle="Aviso de Privacidad”. Usted, por el hecho de enviar información por medio de esta dirección de correo electrónico, autoriza a la Inmobiliaria a administrar sus datos personales y a transferir los mismos a terceros cuando sea estrictamente necesario para otorgarle nuestros servicios. Usted podrá ejercer los derechos de acceso, rectificación, cancelación, oposición, divulgación y limitación de uso, mediante escrito dirigido al Responsable de Protección de Datos Personales, en el domicilio de la Inmobiliaria o mediante correo electrónico {pendiente}} Cualquier modificación que se realice al presente Aviso de Privacidad, será notificada a través de nuestra página de Internet: www.wilsonbienesraices.com"
+      title="Wilson Bienes Raíces"
+      subtitle="Encuéntranos en Facebook"
       button={
-        <Link href="">
+        <Link href="https://www.facebook.com/wilsonbienesraicesmx">
           <a>
             <Button>Facebook</Button>
           </a>
